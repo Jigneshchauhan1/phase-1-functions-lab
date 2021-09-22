cc783ef23774a2f8097a42c1ea4ecf526446e575
Jignesh <jigneshmailbag@gmail.com>
2021-09-21 21:31:22 -0400

    commit tree 46859c09209f704dac96a36f79ad2f3c92827040

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    let headquarter = 42;
+    if (headquarter > pickupLocation){
+        return headquarter - pickupLocation
+    } else (headquarter < pickupLocation);{
+        return pickupLocation - headquarter
+    } 
+
+}
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+
+}
+function distanceTravelledInFeet(headquarter, pickupLocation){
+    if (headquarter > pickupLocation){
+        return (headquarter - pickupLocation) * 264
+    } else (headquarter < pickupLocation);{
+        return (pickupLocation - headquarter) * 264
+    }
+
+}
+function calculatesFarePrice(headquarter, pickupLocation){
+    if (distanceFromHqInFeet(headquarter, pickupLocation) <= 400){
+        return 0;
+     } else if (distanceTravelledInFeet(headquarter, pickupLocation)> 400 && distanceTravelledInFeet(headquarter, pickupLocation)<= 2000){
+         return ((distanceTravelledInFeet(headquarter, pickupLocation)- 400) * 0.02 )
+     } else if (distanceTravelledInFeet(headquarter, pickupLocation)> 2000 && distanceTravelledInFeet(headquarter, pickupLocation)<= 2500){
+         return 25
+     } else if (distanceTravelledInFeet(headquarter, pickupLocation)> 2500){
+         return 'cannot travel that far'
+     } 
+
+}
+    
+
